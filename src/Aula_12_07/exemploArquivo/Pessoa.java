67d28613585448c99a42600cdd74070a9ad8c3b5
Ryan <ryan10.gugu@gmail.com>
2024-07-12 11:32:13 -0300

    Mensagem de comit 5: Aula 23, dia 12/07/24 Sobre Arquivos e exceções

--- src/Aula_12_07/exemploArquivo/Pessoa.java
+++ src/Aula_12_07/exemploArquivo/Pessoa.java
@@ -1,12 +1,17 @@
-package Aula_05_07.exemploPolimorfismo;
+package Aula_12_07.exemploArquivo;
 
-public class Pessoa {
+import java.io.Serializable;
+
+public class Pessoa implements Serializable {
+
     private String nome;
+
     private String sobrenome;
 
     public String getNome(){
         return nome;
     }
+
     public String getSobrenome(){
         return sobrenome;
     }
@@ -14,18 +19,11 @@ public class Pessoa {
     public void setNome(String nome){
         this.nome = nome;
     }
+
     public void setSobrenome(String sobrenome){
         this.sobrenome = sobrenome;
     }
 
-    //Se o método obterDados for 'final' ele não poderá ter o comportamento
-    //substituido em outra class, ou seja, ele não recebe Override;
-
-    //public final String obterDados()
-
-    public String obterDados(){
-        return "Nome: "+nome+" "+sobrenome;
-    }
     @Override
     public String toString(){
         return "Nome: "+nome+" "+sobrenome;
